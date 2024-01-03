b39d480f35efd1c6715f9738f200abec1783b301
syddf <582415830@qq.com>
2024-01-04 01:06:40 +0800

    Add LoL VGC Format.

--- config/formats.ts
+++ config/formats.ts
@@ -34,7 +34,7 @@ export const Formats: FormatList = [
 		searchShow: true,
 		gameType: 'doubles',
 		challengeShow: false,
-		ruleset: ['lolmodvgcrule', '!! Adjust Level = 50', 'Min Source Gen = 9', 'VGC Timer', 'Force Open Team Sheets'],
+		ruleset: ['lolmodvgcrule', '!! Adjust Level = 50', 'VGC Timer', 'Force Open Team Sheets'],
 		banlist: [],
 	},	
 	// S/V Singles
